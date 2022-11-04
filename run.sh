31e73f9401f5a6847720da57a7f539e4a6f0d2f3
SOKHNAAWAA <97348410+SOKHNAAWAA@users.noreply.github.com>
2022-11-04 15:28:16 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ docker pull delaugustin/rna-star:2.7.10a
 docker pull delaugustin/subread:2.0.3
 docker pull delaugustin/fastqc:0.11.9
 docker pull delaugustin/r_with_desqeq2:4.2.1
-ocker pull delaugustin/sra-toolkit:2.11.3
+docker pull delaugustin/sra-toolkit:2.11.3
 
 ## running command 
 mkdir nextflow
