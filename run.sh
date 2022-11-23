68832f4db6fe4ca03aeeab5332078478ede43df1
braaist <ruslan0gumerov@gmail.com>
2022-11-23 20:31:50 +0100

    run.sh is updated (automatical installation of nextflow and docker)

--- run.sh
+++ run.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
-## groupe 1 Projet hackaton
 
+## groupe 1 Projet hackaton
+## create working dir
+mkdir REPROHACK
+cd REPROHACK/
+
+
+##check that nextflow installed
+if ! command -v nextflow &> /dev/null
+then
+    echo "Nextflow installation"
+    sudo apt install default-jre
+    curl -fsSL get.nextflow.io | bash
+    sudo mv nextflow /usr/local/bin
+    exit
+fi
+
+if ! command -v docker &>? /dev/null/
+then
+    echo "Docker installation"
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    exit
+fi
+
+##download script.nf
+wget https://raw.githubusercontent.com/braaist/REPROHACK/main/NEXTFLOW/script.nf
 
 ##installing the images
 
@@ -11,5 +35,4 @@ docker pull delaugustin/r_with_desqeq2:4.2.1
 docker pull delaugustin/sra-toolkit:2.11.3
 
 ## running command 
-mkdir nextflow
-nextflow REPROHACK/NEXTFLOW/script.nf -resume
+nextflow script.nf -resume
